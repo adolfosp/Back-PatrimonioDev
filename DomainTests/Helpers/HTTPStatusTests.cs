d24e5dda412f15f9fcf4952b05651132cf3752ec
Adolfo Poiatti <adolfo.poiatti@inovafarma.com.br>
2021-12-22 22:21:29 -0300

    test: adiciona test da classe http status

---
+++ DomainTests/Helpers/HTTPStatusTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Helpers.Tests
+{
+    [TestClass()]
+    public class HTTPStatusTests
+    {
+        [TestMethod()]
+        public void Retorna_status_204_se_classe_for_nula()
+        {
+            //Arrange & Act
+            var sut = HTTPStatus.RetornaStatus(null);
+
+            //Assert
+            Assert.AreEqual(204, sut);
+        }
+    }
+}
