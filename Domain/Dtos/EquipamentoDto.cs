ac24b376d9130d6484e5d946bbc60ad61f77986d
Adolfo <adolfo8799@gmail.com>
2022-03-17 18:33:59 -0300

    fix: altera tipo retorno ObterTodosEquipamento

--- Domain/Dtos/EquipamentoDto.cs
+++ Domain/Dtos/EquipamentoDto.cs
@@ -1,19 +1,14 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-
-namespace Aplicacao.Dtos
+﻿namespace Aplicacao.Dtos
 {
     public class EquipamentoDto
     {
-        [Required(ErrorMessage = "A descrição do equipamento precisa ser preenchida.")]
-        [MaxLength(20, ErrorMessage = "É necessário informar no máximo 20 caracteres")]
+        public int? CodigoTipoEquipamento { get; set; }
         public string TipoEquipamento { get; set; }
-
-        [Required(ErrorMessage = "É necessário informar o fabricante.")]
         public int CodigoFabricante { get; set; }
-
-        [Required(ErrorMessage = "É necessário informar a categoria.")]
+        public string? NomeFabricante { get; set; }
         public int CodigoCategoria { get; set; }
+        public string? NomeCategoria { get; set; }
+
 
     }
 }
